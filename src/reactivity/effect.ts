4213ed9c1e554839fba56310f970eab6ee7d1e1c
Pineapple <1244476905@qq.com>
2022-01-10 23:14:45 +0800

    add stop, readonly, isReactive, isReadonly

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,16 +1,51 @@
+import { extend } from "../shared";
+
+let activeEffect;
+let shouldTrack;
 class ReactiveEffect {
   private _fn: any;
+  deps = [];
+  active = true;
+  onStop?: () => void;
   constructor(fn, public scheduler?) {
     this._fn = fn;
   }
   run() {
+    if (!this.active) {
+      return this._fn();
+    }
+
+    shouldTrack = true;
     activeEffect = this;
-    return this._fn();
+
+    const result = this._fn();
+    shouldTrack = false;
+
+    return result;
   }
+  stop() {
+    if (this.active) {
+      cleanupEffect(this);
+      if (this.onStop) {
+        this.onStop();
+      }
+      this.active = false;
+    }
+  }
+}
+
+function cleanupEffect(effect) {
+  effect.deps.forEach((dep: any) => {
+    dep.delete(effect);
+  });
+
+  effect.deps.length = 0;
 }
 
 const targetMap = new Map();
 export function track(target, key) {
+  if (!isTracking()) return;
+
   let depsMap = targetMap.get(target);
   if (!depsMap) {
     depsMap = new Map();
@@ -22,7 +57,14 @@ export function track(target, key) {
     dep = new Set();
     depsMap.set(key, dep);
   }
+
+  if (dep.has(activeEffect)) return;
   dep.add(activeEffect);
+  activeEffect.deps.push(dep);
+}
+
+function isTracking() {
+  return shouldTrack && activeEffect !== undefined;
 }
 
 export function trigger(target, key) {
@@ -37,10 +79,18 @@ export function trigger(target, key) {
   }
 }
 
-let activeEffect;
 export function effect(fn, options: any = {}) {
   const _effect = new ReactiveEffect(fn, options.scheduler);
+  extend(_effect, options);
 
   _effect.run();
-  return _effect.run.bind(_effect);
+
+  const runner: any = _effect.run.bind(_effect);
+  runner.effect = _effect;
+
+  return runner;
+}
+
+export function stop(runner) {
+  runner.effect.stop(runner);
 }
--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,18 +1,26 @@
-import { track, trigger } from "./effect";
+import { mutableHandlers, readonlyHandlers } from "./baseHandlers";
+
+export const enum ReactiveFlags {
+  IS_REACTIVE = "__v_isReactive",
+  IS_READONLY = "__v_isReadonly",
+}
 
 export function reactive(raw) {
-  return new Proxy(raw, {
-    get(target, key) {
-      const res = Reflect.get(target, key);
+  return createReactiveHandlers(raw, mutableHandlers);
+}
 
-      track(target, key);
-      return res;
-    },
-    set(target, key, value) {
-      const res = Reflect.set(target, key, value);
+export function readonly(raw) {
+  return createReactiveHandlers(raw, readonlyHandlers);
+}
 
-      trigger(target, key);
-      return res;
-    },
-  });
+export function isReactive(value) {
+  return !!value[ReactiveFlags.IS_REACTIVE];
+}
+
+export function isReadonly(value) {
+  return !!value[ReactiveFlags.IS_READONLY];
+}
+
+function createReactiveHandlers(target, baseHandlers) {
+  return new Proxy(target, baseHandlers);
 }
