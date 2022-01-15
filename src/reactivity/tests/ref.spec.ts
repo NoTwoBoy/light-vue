49a0982467536e56cf9b485c5dd141f631e21a43
Pineapple <1244476905@qq.com>
2022-01-16 00:02:01 +0800

    add proxyRefs

--- src/reactivity/tests/ref.spec.ts
+++ src/reactivity/tests/ref.spec.ts
@@ -1,5 +1,6 @@
 import { effect } from "../effect";
-import { ref } from "../ref";
+import { isReactive, reactive } from "../reactive";
+import { ref, isRef, unRef, proxyRefs } from "../ref";
 
 describe("ref", () => {
   it("happy path", () => {
@@ -37,5 +38,46 @@ describe("ref", () => {
     expect(dummy).toBe(1);
     a.value.count = 2;
     expect(dummy).toBe(2);
+  });
+
+  it("isRef", () => {
+    const a = ref(1);
+    const user = reactive({
+      age: 1,
+    });
+    expect(isRef(a)).toBe(true);
+    expect(isRef(a.value)).toBe(false);
+    expect(isRef(user)).toBe(false);
+  });
+
+  it("usRef", () => {
+    const a = ref(1);
+    const user = ref({
+      age: 1,
+    });
+    expect(unRef(a)).toBe(1);
+    expect(unRef(a.value)).toBe(1);
+    // 在pr阶段
+    // expect(isReactive(unRef(user))).toBe(false);
+  });
+
+  it("proxyRefs", () => {
+    const user = {
+      age: ref(10),
+      name: "pineapple",
+    };
+
+    const proxyRefUser = proxyRefs(user);
+    expect(user.age.value).toBe(10);
+    expect(proxyRefUser.age).toBe(10);
+    expect(proxyRefUser.name).toBe("pineapple");
+
+    proxyRefUser.age = 20;
+    expect(proxyRefUser.age).toBe(20);
+    expect(user.age.value).toBe(20);
+
+    proxyRefUser.age = ref(10);
+    expect(proxyRefUser.age).toBe(10);
+    expect(user.age.value).toBe(10);
   });
 });
